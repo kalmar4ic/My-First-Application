e0f3b0652cbc97ebe44db746af571feeb5b491fb
kalmar4ic <serjcherkashin228@gmail.com>
2024-03-16 23:26:28 +0700

    Initial Commit

---
+++ Rand/RandApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandApp.swift
+//  Rand
+//
+//  Created by Сергей Черкашин on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct RandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
