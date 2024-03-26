2f8df08a5341a72f16f00ceaa2955a2e8c885deb
kalmar4ic <serjcherkashin228@gmail.com>
2024-03-26 22:13:56 +0700

    Сделал запоминание значений из Values и добавил вывод .alert

--- Rand/ContentView.swift
+++ Rand/ContentView.swift
@@ -8,20 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert = false
     var body: some View {
         VStack {
             
             Button("Рандомить", action: {
-                
-                for _ in 0...5{
-                    var r = rand()
-                    print(r)
+                let choise = rand()
+                if choise == 1
+                {
+                    print(const.positive)
                     
+                } else if choise == 2
+                {
+                    print(const.negative)
+                    
+                } else if choise == 3
+                {
+                    print(const.absolutely)
+                    
+                } else if choise == 4
+                {
+                    print(const.badchance)
+                    
+                } else if choise == 5
+                {
+                    print(const.dontunderstand)
                 }
+                
+                showAlert = true
             })
         
                 .buttonStyle(.bordered)
                 .tint(.purple)
+            
+                .alert(isPresented: $showAlert) {
+                            Alert(title: Text("Предупреждение"), message: Text("Это предупреждение об ошибке"), dismissButton: .default(Text("OK")))
+                        }
         }
     }
 }
