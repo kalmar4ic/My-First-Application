cb14da3dd86fb7b384a60a7ab0e2a952de2d1577
kalmar4ic <serjcherkashin228@gmail.com>
2024-03-26 22:12:07 +0700

    Создал файл, в котором будут храниться константы для вывода

--- Rand/Values.swift
+++ Rand/Values.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+struct const {
+    static let positive: String = "Да"
+    static let negative: String = "Нет"
+    static let absolutely: String = "Абсолютно точно"
+    static let badchance: String = "Плохие шансы"
+    static let dontunderstand: String = "Вопрос не ясен"
+}
+
